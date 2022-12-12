c2b79a37213bd78483382d3028bb077e3bcef506
SarthakKalr <104204863+SarthakKalr@users.noreply.github.com>
2022-12-12 18:34:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Prediction1= "";
+Prediction2="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera= document.getElementById("camera");
+Webcam.attach('#camera')
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'">'
+
+    });
+}
+console.log("ml5 version:",ml5.version)
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/fDPS1ISed/model.json");
+function model_loaded(){
+    console.log("mode_loaded")
+}
+
+
